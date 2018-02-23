a614fe1988e73fc7f6ac703fe4bd3c35b072e3fe
Gustavo Godoi <gugodoi@gmail.com>
2018-02-23 09:59:50 -0300

    Game card hover effect

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -9,7 +9,7 @@ const GameCard = ({ game }) => {
           <img className="game-image" src={game.front_box_art} alt="" />
         </div>
         <div className="game-title">{game.title}</div>
-        <div className="game-price">{game.ca_price}</div>
+        <div className="game-price">U$ {game.ca_price}</div>
       </div>
     </div>
   );
