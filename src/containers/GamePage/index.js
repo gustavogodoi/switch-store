155e32d12e2b4da5698148b1a4e30edaa1ad3e29
Gustavo Godoi <gugodoi@gmail.com>
2018-02-27 10:24:50 -0300

    Removing duplicated Header

--- src/containers/GamePage/index.js
+++ src/containers/GamePage/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Header from '../../components/Header';
 import GameInfo from '../../components/GameInfo';
 
 export class GamePage extends Component {
