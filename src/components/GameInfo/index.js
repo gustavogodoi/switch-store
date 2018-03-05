14c97ed72e5036cfbee4b5a6b37b49de3d0f644a
Caio Godoi <caio.godoi@gmail.com>
2018-03-05 15:48:05 -0300

    game-page exemple

--- src/components/GameInfo/index.js
+++ src/components/GameInfo/index.js
@@ -2,7 +2,39 @@ import React from 'react';
 import './GameInfo.css';
 
 const GameInfo = ({ game }) => {
-  return <div className="game-title">Game Page</div>;
+  return (
+    <div className="gamepage-master">
+      <div className="gamepage-title">Game Title</div>
+      <div className="gamepage-image">
+        <img
+          className="the-image"
+          alt=""
+          src="https://cdn.mos.cms.futurecdn.net/2YBLrRBS3KrMr8ffZrkVzi-1200-80.jpg"
+        />
+      </div>
+      <div className="gamepage-description">
+        GOut too the been like hard off. Improve enquire welcome own beloved
+        matters her. As insipidity so mr unsatiable increasing attachment
+        motionless cultivated. Addition mr husbands unpacked occasion he oh. Is
+        unsatiable if projecting boisterous insensible. It recommend be
+        resolving pretended middleton. Improved own provided blessing may
+        peculiar domestic. Sight house has sex never. No visited raising gravity
+        outward subject my cottage mr be. Hold do at tore in park feet near my
+        case. Invitation at understood occasional sentiments insipidity
+        inhabiting in. Off melancholy alteration principles old. Is do speedily
+        kindness properly oh. Respect article painted cottage he is offices
+        parlors. Rooms oh fully taken by worse do. Points afraid but may end law
+        lasted. Was out laughter raptures returned outweigh. Luckily cheered
+        colonel me do we attacks on highest enabled. Tried law yet style child.
+        Bore of true of no be deal. Frequently sufficient in be unaffected. The
+        furnished she concluded depending procuring concealed. In post mean shot
+        ye. There out her child sir his lived. Design at uneasy me season of
+        branch on praise esteem. Abilities discourse believing consisted
+        remaining to no. Mistaken no me denoting dashwood as screened. Whence or
+        esteem easily he on. Dissuade husbands at of no if disposal.
+      </div>
+    </div>
+  );
 };
 
 export default GameInfo;
