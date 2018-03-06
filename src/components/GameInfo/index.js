73fb17510b9e21cd2758cf5056e304ed4d37fd1b
Caio Godoi <caio.godoi@gmail.com>
2018-03-06 18:50:45 -0300

    GamePage Changes

--- src/components/GameInfo/index.js
+++ src/components/GameInfo/index.js
@@ -4,34 +4,15 @@ import './GameInfo.scss';
 const GameInfo = ({ game }) => {
   return (
     <div className="gamepage-master">
-      <div className="gamepage-title">Game Title</div>
+      <div className="gamepage-title">{game.slug}</div>
       <div className="gamepage-image">
-        <img
-          className="the-image"
-          alt=""
-          src="https://cdn.mos.cms.futurecdn.net/2YBLrRBS3KrMr8ffZrkVzi-1200-80.jpg"
-        />
+        <img className="the-image" alt="" src={game.front_box_art} />
       </div>
-      <div className="gamepage-description">
-        GOut too the been like hard off. Improve enquire welcome own beloved
-        matters her. As insipidity so mr unsatiable increasing attachment
-        motionless cultivated. Addition mr husbands unpacked occasion he oh. Is
-        unsatiable if projecting boisterous insensible. It recommend be
-        resolving pretended middleton. Improved own provided blessing may
-        peculiar domestic. Sight house has sex never. No visited raising gravity
-        outward subject my cottage mr be. Hold do at tore in park feet near my
-        case. Invitation at understood occasional sentiments insipidity
-        inhabiting in. Off melancholy alteration principles old. Is do speedily
-        kindness properly oh. Respect article painted cottage he is offices
-        parlors. Rooms oh fully taken by worse do. Points afraid but may end law
-        lasted. Was out laughter raptures returned outweigh. Luckily cheered
-        colonel me do we attacks on highest enabled. Tried law yet style child.
-        Bore of true of no be deal. Frequently sufficient in be unaffected. The
-        furnished she concluded depending procuring concealed. In post mean shot
-        ye. There out her child sir his lived. Design at uneasy me season of
-        branch on praise esteem. Abilities discourse believing consisted
-        remaining to no. Mistaken no me denoting dashwood as screened. Whence or
-        esteem easily he on. Dissuade husbands at of no if disposal.
+      <div className="gamepage-release">
+        Data de Lançamento: {game.release_date}
+      </div>
+      <div className="gamepage-price">
+        Preço: {game.ca_price && `U$ ${game.ca_price}`}
       </div>
     </div>
   );
