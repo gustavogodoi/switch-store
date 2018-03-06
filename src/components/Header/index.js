80419bbb4005179611fe3839302b157f7b707362
Gustavo Godoi <gugodoi@gmail.com>
2018-03-06 16:49:55 -0300

    Incluindo link no header para  a página principal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 
 const Header = () => {
   return (
     <div className="header-master">
-      <div className="header-nintendo-image">
+      <Link to="/" className="header-nintendo-image">
         <img
           className="nintendo-image"
           alt=""
           src="https://cdn.mos.cms.futurecdn.net/2YBLrRBS3KrMr8ffZrkVzi-1200-80.jpg"
         />
-      </div>
+      </Link>
       <div className="header-title">
         <div>
-          <h1>Nintendo Switch Games</h1>
+          <h1>
+            <Link to="/">Nintendo Switch Games</Link>
+          </h1>
         </div>
       </div>
       <div className="header-search">
